105b8586821410711d1625a1a12fe51ade04d8d9
coco <56118249+cocovs@users.noreply.github.com>
2026-04-14 22:28:23 +0800

    assemble-flow v0.2.1

--- Formula/assemble-flow.rb
+++ Formula/assemble-flow.rb
@@ -1,25 +1,25 @@
 class AssembleFlow < Formula
   desc "Developer CLI for AssembleFlow workflows"
   homepage "https://github.com/SSYCloud/AssembleFlow"
-  version "0.1.3"
+  version "0.2.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/SSYCloud/AssembleFlow/releases/download/v0.1.3/assemble-flow-darwin-arm64.tar.gz"
-      sha256 "36b0cade38ecef38041be8b53dafa0ee6c84c92bba5d7a6bbda901c2bd56d0e3"
+      url "https://github.com/SSYCloud/AssembleFlow/releases/download/v0.2.1/assemble-flow-darwin-arm64.tar.gz"
+      sha256 "cac9e0178a396ec32a31f80a32cdf6bd8e1e0879b8288728f62ae239cdc352f7"
     else
-      url "https://github.com/SSYCloud/AssembleFlow/releases/download/v0.1.3/assemble-flow-darwin-amd64.tar.gz"
-      sha256 "da313b6aa02401b913867b3ab50d32d3a9eefdd638cbc298de4011bf7dd25258"
+      url "https://github.com/SSYCloud/AssembleFlow/releases/download/v0.2.1/assemble-flow-darwin-amd64.tar.gz"
+      sha256 "f4a7c560cc0925a90dd1023ecb4214b9c6c2cff352525a7ecd018846ef44b929"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/SSYCloud/AssembleFlow/releases/download/v0.1.3/assemble-flow-linux-arm64.tar.gz"
-      sha256 "c5f16019109ef8a810e521cfdebfb3bc94333a0e739fef04a89191c7a0f5fb9f"
+      url "https://github.com/SSYCloud/AssembleFlow/releases/download/v0.2.1/assemble-flow-linux-arm64.tar.gz"
+      sha256 "5b0515d27d247617ed1423f94cb176973f584af18ece4faae50bd7c180b66c5f"
     else
-      url "https://github.com/SSYCloud/AssembleFlow/releases/download/v0.1.3/assemble-flow-linux-amd64.tar.gz"
-      sha256 "4e9face5e2ed4a317fc5b6ef7d3ccdbd05fb206a6d072c081e348239d2e0a417"
+      url "https://github.com/SSYCloud/AssembleFlow/releases/download/v0.2.1/assemble-flow-linux-amd64.tar.gz"
+      sha256 "be5b316c9a8d39cb402ec5bf488cee77c2c5d1998b437823d64ca49a377b6357"
     end
   end
 
